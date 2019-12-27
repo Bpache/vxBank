d2aa31b7773219b9c6e9bd3ec4f18b282c4a565b
unclePeng <187079603@qq.com>
2019-12-27 18:36:50 +0800

    2019年12月27日18:36:39 push for peng

--- eureka-consumer/src/main/java/com/snow/eurekaconsumer/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/snow/eurekaconsumer/EurekaConsumerApplication.java
@@ -3,9 +3,11 @@ package com.snow.eurekaconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
