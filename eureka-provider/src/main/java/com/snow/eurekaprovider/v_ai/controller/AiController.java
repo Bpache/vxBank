69d175c02c7a0c060e45256554e7e360fb049953
unclePeng <187079603@qq.com>
2019-12-30 14:25:14 +0800

    2019年12月30日14:25:04 push for peng

--- eureka-provider/src/main/java/com/snow/eurekaprovider/v_ai/controller/AiController.java
+++ eureka-provider/src/main/java/com/snow/eurekaprovider/v_ai/controller/AiController.java
@@ -5,6 +5,7 @@ import com.snow.eurekaprovider.v_ai.model.AI;
 import com.snow.eurekaprovider.v_ai.service.IAIservice;
 import com.snow.vo.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,21 +20,53 @@ public class AiController {
 
     private JsonData jd;
 
-    @RequestMapping("/get")
-    public JsonData getAi(Integer aiId) {
-        AI obj = aIservice.selectByPrimaryKey(aiId);
+    @RequestMapping("/add")
+    public JsonData addAi(@RequestBody AI ai) {
+        int n = aIservice.insert(ai);
         jd = new JsonData();
-        jd.setResult(obj);
+        String msg = "操作成功！";
+        if(n<1){
+            msg = "操作失败！";
+        }
+        jd.setMessage(msg);
         return jd;
     }
 
+    @RequestMapping("/del")
+    public JsonData delAi(@RequestBody AI ai) {
+        int n = aIservice.deleteByPrimaryKey(ai.getAiId());
+        jd = new JsonData();
+        String msg = "操作成功！";
+        if(n<1){
+            msg = "操作失败！";
+        }
+        jd.setMessage(msg);
+        return jd;
+    }
+
+
     @RequestMapping("/list")
-    public JsonData list(AI ai){
+    public JsonData list(@RequestBody AI ai){
         jd = new JsonData();
         List<AI> list = aIservice.load(ai);
         jd.setResult(list);
-
+        String msg = "查询成功！";
+        if(list.size()<1){
+            msg = "查询无结果";
+        }
+        jd.setMessage(msg);
         return jd;
     }
 
+    @RequestMapping("/edit")
+    public JsonData edit(@RequestBody AI ai){
+        int n = aIservice.updateByPrimaryKey(ai);
+        jd = new JsonData();
+        String msg = "操作成功！";
+        if(n<1){
+            msg = "操作失败！";
+        }
+        jd.setMessage(msg);
+        return jd;
+    }
 }
